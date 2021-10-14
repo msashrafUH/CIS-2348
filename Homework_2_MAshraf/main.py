89d884663400b3580e7eef214f396b5e27bfed6f
msashrafUH <89705797+msashrafUH@users.noreply.github.com>
2021-10-13 22:07:29 -0500

    part c
    Modify your program further so that after parsing all dates from the
    input file “inputDates.txt”, it writes out the correct ones into an
    output file called: “parsedDates.txt”.

--- Homework_2_MAshraf/main.py
+++ Homework_2_MAshraf/main.py
@@ -1,29 +1,39 @@
-userFile = open('inputDates.txt', 'r')
+userFile = open('inputDates.txt','r')
 dates = userFile.readlines()
 
-months = ['January', 'February', 'March', 'April', 'May', 'June','July', 'August', 'September', 'October', 'November', 'December']
+months = ['January','February','March','April','May','June','July','August','September','October','November','December']
 
+count = 0
 for userDate in dates:
-    if userDate == '-1':
+  count += 1
+  if userDate=='-1':
+    break
+
+  try:
+    arr = userDate.split()
+    digit = len(arr)
+    if digit!=3:
+      continue
+    m = arr[0]
+    day = arr[1]
+    y = arr[2]
+    d, comma = day.split(',')
+
+    for x in range(12):
+      if userDate.find(months[x])>=0:
+        mo = str(x+1)
+        ans = mo + '/' + d + '/' + y
+        if count>0:
+          file2 = open('parsedDates.txt','a')
+          file2.write(ans + '\n')
+          file2.close()
+        else:
+          file3 = open('parsedDates.txt','w')
+          file3.write(ans + '\n')
+          file3.close()
         break
 
-    try:
-        arr = userDate.split()
-        digit = len(arr)
-        if digit != 3:
-            continue
-        m = arr[0]
-        day = arr[1]
-        y = arr[2]
-        d, comma = day.split(',')
-
-        for x in range(12):
-            if userDate.find(months[x]) >= 0:
-                mo = str(x + 1)
-                ans = mo + '/' + d + '/' + y
-                print(ans)
-                break
-    except ValueError:
-        continue
+  except ValueError:
+    continue
 
 userFile.close()
