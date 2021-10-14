6e0efabdad73d583d8eddda36177b064357340f2
msashrafUH <89705797+msashrafUH@users.noreply.github.com>
2021-10-13 19:01:28 -0500

    Amended for part b
    After the program is working as above, modify the program so that it
    reads all dates from an input file “inputDates.txt” (an Example file is
    attached).

--- Homework_2_MAshraf/main.py
+++ Homework_2_MAshraf/main.py
@@ -1,28 +1,29 @@
-var = 0
-months = ['January','February','March','April','May','June','July','August','September','October','November','December']
-while var == 0:
-  try:
-    userDate = input("Enter the month day, year: ")
-  except SyntaxError:
-    continue
-  if userDate=='-1':
-    break
+userFile = open('inputDates.txt', 'r')
+dates = userFile.readlines()
 
-  try:
-    arr = userDate.split()
-    digit = len(arr)
-    if digit!=3:
-      continue
-    m = arr[0]
-    day = arr[1]
-    y = arr[2]
-    d, comma = day.split(',')
+months = ['January', 'February', 'March', 'April', 'May', 'June','July', 'August', 'September', 'October', 'November', 'December']
 
-    for x in range(12):
-      if userDate.find(months[x])>=0:
-        mo = str(x+1)
-        ans = mo + '/' + d + '/' + y
-        print(ans)
+for userDate in dates:
+    if userDate == '-1':
         break
-  except ValueError:
-    continue
+
+    try:
+        arr = userDate.split()
+        digit = len(arr)
+        if digit != 3:
+            continue
+        m = arr[0]
+        day = arr[1]
+        y = arr[2]
+        d, comma = day.split(',')
+
+        for x in range(12):
+            if userDate.find(months[x]) >= 0:
+                mo = str(x + 1)
+                ans = mo + '/' + d + '/' + y
+                print(ans)
+                break
+    except ValueError:
+        continue
+
+userFile.close()
